9d7a7f3a5d7bcb9ca08a3f101f294b29c4d35610
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2024-03-05 23:56:45 +0600

    all done

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -11,13 +11,13 @@ const Profile: NextPage = () => {
   return loading ? (
     <ProfileSkeleton />
   ) : (
-    <div className="max-w-lg mx-auto my-10 bg-white rounded-lg shadow-md p-5">
+    <div className="max-w-lg mx-auto my-10 bg-slate-800 rounded-lg shadow-md p-5">
       <h2 className="text-center text-2xl font-semibold mt-3">{user?.name}</h2>
-      <p className="text-center text-gray-600 mt-1">{user?.email}</p>
+      <p className="text-center text-gray-300 mt-1">{user?.email}</p>
 
       <div className="mt-5">
         <h3 className="text-xl font-semibold">Bio</h3>
-        <p className="text-gray-600 mt-2">
+        <p className="text-gray-300 mt-2">
           John is a software engineer with over 10 years of experience in
           developing web and mobile applications. He is skilled in JavaScript,
           React, and Node.js.
